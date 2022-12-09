461de8f252afa48f624aa85932d0c9bbc306bc58
Mohammodjuwelislam <mohammodjuwel61@gmail.com>
2022-12-10 01:08:03 +0600

    style-change comment commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,50 @@
 
 
 	jQuery(document).ready(function($) {
+		//slider-active
 		$('.slider-active').owlCarousel({
 			loop:true,
 			nav:true,
 			navText:['<img src="assets/img/left-arrow.png" alt="">','<img src="assets/img/right-arrow.png" alt="">'],
+			dots:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1000:{
+					items:1
+				}
+			}
+		});
+		//masonary
+		$('.product-list').masonry({
+			// options
+		});
+
+		//testimonial-slides-active
+		$('.testimonial-slides-active').owlCarousel({
+			loop:true,
+			nav:false,
+			dots:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1000:{
+					items:1
+				}
+			}
+		});
+
+		$('.brand-slides-active').owlCarousel({
+			loop:true,
+			nav:false,
 			dots:true,
 			responsive:{
 				0:{
